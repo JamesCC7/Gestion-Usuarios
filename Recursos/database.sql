f99fdcbbb9e99ec9122484f2bc459b75a31843fc
Jaime Cepeda <jr.cepeda.cc@gmail.com>
2023-02-21 18:37:08 -0500

    Creacion tabla ususario

--- Recursos/database.sql
+++ Recursos/database.sql
@@ -2,3 +2,11 @@ create database gestion_usuarios;
 
 use gestion_usuarios;
 
+CREATE TABLE usuarios (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  apellido varchar(60) NOT NULL,
+  email varchar(60) NOT NULL,
+  nombre varchar(60) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY UK_kfsp0s1tflm1cwlj8idhqsad0 (email)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
